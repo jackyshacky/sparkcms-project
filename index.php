3ed05cb4fb55d2b0d695c08230a7a5e62e6fbcbd
jackyshacky <jackvijaydhar1994@gmail.com>
2017-07-31 19:43:36 +0000

    Second Commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE <!DOCTYPE html>
+<html>
+    <head>
+        <title>SparkCMS - The Lightweight Open Source Project</title>
+    </head>
+    
+    <body>
+        <h1>Welcome To SparkCMS</h1>
+        <img src="http://static1.squarespace.com/static/56421346e4b0d582d7541661/t/564486bbe4b00ca829e0ec5c/1499099888936/"></img>
+    </body>
+</html>
