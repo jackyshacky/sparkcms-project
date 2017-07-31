ac84d91ddb4d6f23d43e375c233a6d26b5d06569
jackyshacky <jackvijaydhar1994@gmail.com>
2017-07-31 19:05:19 +0000

    First Commit

---
+++ hello-world.php
@@ -0,0 +1,15 @@
+<html><body>
+<?php
+// A simple web site in Cloud9 that runs through Apache
+// Press the 'Run' button on the top to start the web server,
+// then click the URL that is emitted to the Output tab of the console
+
+$arr = array("A", "B" );
+
+echo "<pre>";
+print_r($arr);
+echo "<pre>";
+
+?>
+</body>
+</html>
